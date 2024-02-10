d9c0dee495bd5cc11182028a8aa7ef1fb8598c07
Alan <alann678n@gmail.com>
2024-02-10 12:38:54 -0300

    Feat: Note filtering by search

--- notes-React/src/app.tsx
+++ notes-React/src/app.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 import logo from './assets/logo-nlw-home.svg'
 import { NewNote } from './components/new_note_card'
 import { NoteCard } from './components/note_card'
@@ -11,7 +11,20 @@ interface Note{
 }
 
 export function App() {
-  const [notes, setNotes] = useState<Note[]>([])
+
+  const [search, setSearch] = useState('')
+
+
+
+  const [notes, setNotes] = useState<Note[]>(()=>{
+
+    const notesOnStorage = localStorage.getItem('notes')
+
+    if(notesOnStorage){
+      return JSON.parse(notesOnStorage)
+    }
+    return []
+  })
 
   function onNoteCreation(content: string){
     const new_note = {
@@ -20,8 +33,22 @@ export function App() {
       content,
     }
 
+    const notesArray = [new_note, ...notes]
+
     setNotes([new_note, ...notes])
+
+    localStorage.setItem('notes', JSON.stringify(notesArray))
   }
+
+  function handleSearch(event: ChangeEvent<HTMLInputElement>){
+    const query = event.target.value
+
+    setSearch(query)
+  }
+
+  const filteredNotes = search != '' 
+  ? notes.filter(note => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
+  : notes
 
   return (
     <div className='mx-auto max-w-6xl my-12 space-y-6'>
@@ -34,7 +61,9 @@ export function App() {
         <input 
           type='text' 
           placeholder='Busque em suas notas...'
-          className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder: text-slate-500'/>
+          className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder: text-slate-500'
+          onChange = {handleSearch}/>
+          
       </form>
       
       {/*Outline de divisao*/}
@@ -44,7 +73,7 @@ export function App() {
 
         <NewNote onNoteCreation= {onNoteCreation}/>
 
-        {notes.map(
+        {filteredNotes.map(
           note => {
             return <NoteCard  key={note.id}   note={note}/>
           }
@@ -52,4 +81,5 @@ export function App() {
       </div>
     </div>
   )
+
 }
